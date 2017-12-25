0a3f041f95d83426b7afdd5bf4511b3832e8b14b
macbook <mac>
2017-12-26 00:49:30 +0100

    Add NetworkManager

---
+++ Robot/NetworkManager.h
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.h
+//  Robot
+//
+//  Created by macbook on 26/12/2017.
+//  Copyright © 2017 bessem. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetworkManager : NSObject
+
+@end
---
+++ Robot/NetworkManager.m
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.m
+//  Robot
+//
+//  Created by macbook on 26/12/2017.
+//  Copyright © 2017 bessem. All rights reserved.
+//
+
+#import "NetworkManager.h"
+
+@implementation NetworkManager
+
+@end
