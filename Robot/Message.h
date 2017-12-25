8bb4dab2cac8511e557152f688acdd7b78717100
macbook <mac>
2017-12-25 21:20:28 +0100

    first commit

---
+++ Robot/Message.h
@@ -0,0 +1,14 @@
+//
+//  Message.h
+//  Robot
+//
+//  Created by macbook on 25/12/2017.
+//  Copyright © 2017 bessem. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Message : NSObject
+
+
+@end
---
+++ Robot/Message.m
@@ -0,0 +1,13 @@
+//
+//  Message.m
+//  Robot
+//
+//  Created by macbook on 25/12/2017.
+//  Copyright © 2017 bessem. All rights reserved.
+//
+
+#import "Message.h"
+
+@implementation Message
+
+@end
